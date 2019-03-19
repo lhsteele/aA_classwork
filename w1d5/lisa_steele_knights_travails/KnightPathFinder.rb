0886515f0e8d5cbb4e805f08ceee7cec84e1e22c
Lisa Steele <lisahsteele@gmail.com>
2019-03-18 22:29:46 -0700

    trying to figure out where I went wrong

--- w1d5/lisa_steele_knights_travails/KnightPathFinder.rb
+++ w1d5/lisa_steele_knights_travails/KnightPathFinder.rb
@@ -1,21 +1,38 @@
 require_relative "PolyTreeNode.rb"
 require "byebug"
 
+#questions:
+#I'm not sure where the root_node attribute has come from?
+#The solutions list it under attr_accessor, but is it right, where I've put it in initialize?
+
+#the error that I'm getting - my build_move_tree isn't being created properly?
+#"undefined method 'bfs' for [0, 0]:Array"
+# printing poss_moves on line 66 shows I'm not 
+
+#I didn't understand the instructions that the instance was to be initialized in build_move_tree
+# going forward, how can I work to understand these types of instructions better?
+
 class KnightPathFinder 
   attr_reader :pos, :start_pos
+  attr_accessor :root_node
 
-  def initialize(pos)
-    @pos = pos 
-    @start_pos = PolyTreeNode.new(pos)
+  #I had created a @pos and @start_pos, which were doing the same thing but 
+  # I wasn't using @pos (which was storing the position passed in)
+  def initialize(start_pos)
+    #@pos = pos 
+    @start_pos = start_pos
     @considered_positions = [@start_pos]
+    @root_node 
+
+    #build_move_tree should be called here 
+    build_move_tree
   end
 
-  def inspect 
-    {"root_node" => @root_node, "parent" => @parent}
-  end
 
+  #here, like in the solution, I am generating only positions (not instances)
   def self.valid_moves(starting_pos)
     x, y = starting_pos 
+
     poss_moves = []
     possibilities = [[2, -1], [2, 1], [1, 2], [1, -2], [-2, 1], [-2, -1], [-1, -2], [-1, 2]]
     possibilities.each do |poss|
@@ -24,75 +41,75 @@ class KnightPathFinder
 
       if new_move.first >= 0 && new_move.first <= 7 && new_move.last >= 0 && new_move.last <= 7
         if new_move.first >= x && new_move.last >= y
-          poss_moves += [new_move] 
+          poss_moves += [new_move]
         end
-      else 
-        break 
       end
     end
     poss_moves
   end
 
-  def new_move_positions(starting_node)
-    starting_pos = starting_node.root_node
-    poss_moves = KnightPathFinder.valid_moves(starting_pos).reject { |move| @considered_positions.include?(move) }
-    #@considered_positions += poss_moves
-    poss_moves 
+  def new_move_positions(starting_pos)
+    KnightPathFinder.valid_moves(starting_pos).reject { |move| @considered_positions.include?(move) }
   end
   
   def build_move_tree
-    queue = [@start_pos]
-    #queue = [start_pos.root_node]
+    #they didn't instantiate a new instance of this class until here.
+    self.root_node = PolyTreeNode.new(start_pos)
+    queue = [root_node]
+ 
     until queue.empty?
-      current_node = queue.shift 
-      
-      poss_moves = new_move_positions(current_node)
 
-      #I think I'm ending up with duplicates because of something in this loop
-      #it seems to be adding on to the queue (line 56) before anything gets shifted off
-      #not sure why as it shouldn't be generating new positions until line 49, which means
-      #I should have exited the loop on lines 55-60
+      current_node = queue.shift 
+      current_value = current_node.root_node
+   
+      poss_moves = new_move_positions(current_value)
+      print poss_moves
       poss_moves.each do |move|
         move_node = PolyTreeNode.new(move) 
         move_node.parent = current_node 
         queue << move_node
-        p queue
-        @considered_positions << move_node 
       end
     end
-    @considered_positions 
   end
 
+
+  #had the build_move_tree in here which is wrong
+  #arr should be set with self inside , not an empty array
   def bfs(target_pos) #this is called on the root node 
     tar_x, tar_y = target_pos 
-    build_move_tree
-    arr = []
-    #debugger 
-    @considered_positions.each do |position|
-      arr << position.root_node
-    end
-    p arr
-    path = []
+
+    arr = [self]
+
     until arr.empty?
-     
       current_check = arr.shift 
-
-      root_x, root_y = current_check
-
-      if root_x == tar_x && root_y == tar_y
-        path << current_check
-        p path
-        return #path 
-      end
+      return current_check if current_check.root_node == target_pos
+      arr.concat(current_check.children)
     end
-    path.nil? ? nil : path 
+    nil
   end
+
+  def find_path(end_pos)
+    p @start_pos
+    end_node = @start_pos.bfs(end_pos)
+
+    trace_path_back(end_node)
+  end
+
+  def trace_path_back(end_node)
+    path = []
+    current_node = end_node 
+    until current_node.nil?
+      path.unshift(end_node.parent)
+      current_node = current_node.parent 
+    end
+    p nodes 
+  end
+
 
 end
 
-kpf = KnightPathFinder.new([0,1])
-#kpf.build_move_tree
-kpf.bfs([6, 7])
+kpf = KnightPathFinder.new([0,0])
+#kpf.find_path([6, 7])
 
 
 # a knight moves 2 sq horizontally, then 1 vertically
